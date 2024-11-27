a72a3901490f5b1f410c6090e88cdf2392667b84
Megan White <63659888+meganzastrow@users.noreply.github.com>
2024-11-27 10:45:03 -0500

    Update venue_utilization.py
    Signed-off-by: Megan White <63659888+meganzastrow@users.noreply.github.com>

--- venue_utilization.py
+++ venue_utilization.py
@@ -7,7 +7,7 @@ try:
     import openpyxl
 except ImportError:
     # Install openpyxl if it's not already installed
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "openpyxl"])
+    subprocess.check_call([sys.executable, "pip", "install", "openpyxl"])
     import openpyxl  # Import again after installation
   
 import pandas as pd
